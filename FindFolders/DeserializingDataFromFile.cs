5c47a85799bc806e502b1414a02c3eaa62eac30d
Alexandr Kryuchkov <grey.miller13@gmail.com>
2021-06-26 14:07:26 +0300

    Реализовал класс поиска папок

---
+++ FindFolders/DeserializingDataFromFile.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text.Json;
+
+
+namespace Find_Folders
+{
+    public static class DeserializingDataFromFile
+    {
+        public static void Deserialize(string filePathToDeserialize, out FindFolders obj)
+        {
+            string json = File.ReadAllText(filePathToDeserialize);
+            obj = JsonSerializer.Deserialize<FindFolders>(json);
+        }
+    }
+}
--- FindFolders/FindFolder.cs
+++ FindFolders/FindFolder.cs
@@ -1,12 +1,99 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
-namespace FindFolders
+namespace Find_Folders
 {
-    public class FindFolder
+    public delegate void Message(string str);
+    public class FindFolders
     {
-        public bool Find()
-        { 
-        
+        private static string _userName = Environment.UserName;
+        private string _userPath = $"C:\\Users\\{_userName}\\";
+        //для десериализации данных (части пути)
+        public List<string> directory { get; set; }
+        //после конкатенации строк будет записан полный пусть
+        //private string[] _fullDirectory;
+        private List<string> _fullDirectory;
+        //после поиска будут записаны пути которые были найдены на устройстве
+        private List<string> _findDirectory;
+
+        //Эвент для логирования
+        public event Message Info;
+
+        //Метод создания полного пути
+        private void CreateFullDirectory()
+        {
+            try
+            {
+                _fullDirectory = new List<string>(directory.Count);
+                for (int i = 0; i < directory.Count; i++)
+                {
+                    _fullDirectory.Add(_userPath + directory[i]);
+                }
+            }
+            catch (Exception NullReferenceException)
+            {
+                Info?.Invoke(NullReferenceException.Message);
+            }
+
+        }
+        //Основной метод поиска папок
+        public void SearchFolders()
+        {
+            try
+            {
+                CreateFullDirectory();
+                _findDirectory = new List<string>(_fullDirectory.Count);
+                for (int i = 0; i < _fullDirectory.Count; i++)
+                {
+                    if (Directory.Exists(_fullDirectory[i]))
+                    {
+                        _findDirectory.Add(_fullDirectory[i]);
+                    }
+                }
+            }
+            catch (Exception NullReferenceException)
+            {
+                Info?.Invoke(NullReferenceException.Message);
+            }
+
+        }
+
+        //временный метод для тестирования
+        //в проде необходимо убрать
+        public void ShowDirectoryForSearch()
+        {
+            if (_fullDirectory != null)
+            {
+                foreach (var item in _fullDirectory)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Директории не были созданы");
+            }
+        }
+        //Получение директорий для поиска
+        public List<string> GetDirectoryForSearch()
+        {
+            return _fullDirectory;
+        }
+
+        //временный метод для тестирования
+        //в проде необходимо убрать
+        public void ShowSearchFolders()
+        {
+            foreach (var item in _findDirectory)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        //Получение найденных директорий на устройстве пользователя
+        public List<string> GetSearchFolders()
+        {
+            return _findDirectory;
         }
     }
 }
