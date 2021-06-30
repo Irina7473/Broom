d958977fb11b68d43d06a6f8d17c7e36f0aac9c6
Irina <ten7473@mail.ru>
2021-06-30 22:23:19 +0500

    Добавила возможность создавать несколько журналов в день и пишу события в журналы в зависимости от типа события

--- Logger/LogToFile.cs
+++ Logger/LogToFile.cs
@@ -15,50 +15,57 @@ namespace Logger
         private readonly string loggerDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "BroomLogger");
         //вариант с расположением журналов в папке исполнительного файла
         // private readonly string loggerDir=Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),"BroomLogger");
+        //в разработке вариант с config файлом
 
         public LogToFile()
         {
-            Directory.CreateDirectory(loggerDir);  //создается 1 раз при 1 запуске программы
-            RegistrationPath = Path.Combine(loggerDir, "RegistrationLog.txt");  //создается 1 раз при 1 запуске программы
+            //создается 1 раз при 1 запуске программы
+            Directory.CreateDirectory(loggerDir);
+            //создается 1 раз при 1 запуске программы
+            RegistrationPath = Path.Combine(loggerDir, "RegistrationLog.txt"); 
+            
+            //текущая дата без времени
             var creatTime = DateTime.Now.ToShortDateString();
-            var sLog = "SuccessLog" + creatTime + ".txt";
-            SuccessPath = Path.Combine(loggerDir, sLog); //создается каждый раз при запуске программы
-            var eLog = "ErrorsLog" + creatTime + ".txt";
-            ErrorsPath = Path.Combine(loggerDir, eLog);  //создается каждый раз при запуске программы
+            //проверка количества файлов созданных в текущую дату
+            string[] filesSuccess = Directory.GetFiles(loggerDir, $"SuccessLog {creatTime}*");
+            string[] filesErrors = Directory.GetFiles(loggerDir, $"ErrorsLog {creatTime}*");
+            //номер следующего на текущую дату файла
+            var numberSuccess = filesSuccess.Length + 1;
+            var numberErrors = filesErrors.Length + 1;
+
+            //создаются каждый раз при запуске программы со следующим в текущую дату номером
+            var sLog = "SuccessLog" + " " + creatTime + " " + numberSuccess + ".txt";
+            SuccessPath = Path.Combine(loggerDir, sLog); 
+            var eLog = "ErrorsLog" + " " + creatTime + " " + numberErrors + ".txt";
+            ErrorsPath = Path.Combine(loggerDir, eLog);  
         }
-        
-        public async void RecordEventToFile(string nameFile, string locationFile)
+
+        public async void RecordEventToFile(string type, string message)
         {
-            var text = DateTime.Now + " | " + nameFile + " | " + locationFile + " | " + Environment.UserName + " \n";
+            var text = DateTime.Now + " | " + type + " | " + message + " | " + Environment.UserName + " \n";
+            await File.AppendAllTextAsync(RegistrationPath, text);
+            if (type == "SUCCESS") this.RecordSuccessToFile(message);
+            if (type == "ERROR") this.RecordErrorsToFile(message);
+        }
+            
+        public async void RecordSuccessToFile(string message)
+        {
+            var text = DateTime.Now + " | " + message + " \n";
             await File.AppendAllTextAsync(SuccessPath, text);
         }
 
-        public void ClearToFile()
+        public async void RecordErrorsToFile(string message)
         {
-            if (File.Exists(SuccessPath)) File.WriteAllText(SuccessPath, "");
-            Console.WriteLine("Журнал очищен");
+            var text = DateTime.Now + " | " + message + " \n";
+            await File.AppendAllTextAsync(ErrorsPath, text);
         }
-
-        public void DeleteToFile()
+                        
+        public async void ReadFromFile()
         {
-            if (File.Exists(SuccessPath)) File.Delete(SuccessPath);
-            Console.WriteLine("Журнал удален");
-        }
-
-        public void ReadFromFile()
-        {
-            StreamReader reader = new StreamReader(SuccessPath);
-            Console.WriteLine(reader.ReadToEnd());
-            reader.Close();
-        }
-
-        // асинхронное чтение слишком быстро - не успеваю записать
-        /*public static async void ReadFromFile()
-        {
-            StreamReader reader = new StreamReader(_path);
+            StreamReader reader = new StreamReader(RegistrationPath);
             Console.WriteLine(await reader.ReadToEndAsync());
             reader.Close();
-        }*/
+        }
     }
 
 }
