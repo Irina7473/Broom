09d61378edcc5d86d18ce9991a974f744abf9c04
Irina <ten7473@mail.ru>
2021-07-11 16:13:21 +0500

    Запись в БД сделала аналогично записи в файл

--- DataModel/TotalLog.cs
+++ DataModel/TotalLog.cs
@@ -5,15 +5,15 @@ namespace DataModel
     public class TotalLog
     {
         public int Id { get; set; }
-        public int IdTypeEvent { get; set; }
+        public string TypeEvent { get; set; }
         public string DateTimeEvent { get; set; }
         public string User { get; set; }
         public string Message { get; set; }
 
-        public TotalLog(int id, int idtypeevent, string datetimeevent, string user, string message)
+        public TotalLog(int id, string typeevent, string datetimeevent, string user, string message)
         {
             Id = id;
-            IdTypeEvent = idtypeevent;
+            TypeEvent = typeevent;
             DateTimeEvent = datetimeevent;
             User = user;
             Message = message;
--- Logger/LogToDB.cs
+++ Logger/LogToDB.cs
@@ -11,56 +11,15 @@ namespace Logger
         public DBLogger db = new();
         public SqliteCommand command = new();
 
-        public LogToDB() { }
+        public LogToDB() { }              
 
-        public void AddRecordsTypesOfEvent(string typeevent)
+        public void RecordEventToDB(string typeevent, string message)
         {
             db.Open();
             command.Connection = db._connection;
-            command.CommandText = $"INSERT INTO tab_types_of_events (type_event) VALUES ('{typeevent}')";
+            command.CommandText = $"INSERT INTO tab_total_log (type_event, date_time_event, user, message)" +
+                $"VALUES ('{typeevent}', '{DateTime.Now.ToString()}', '{Environment.UserName.ToString()}', '{message}')";
             command.ExecuteNonQuery();
-            db.Close();
-        }
-
-        public void AddRecordsTotalLog(int idtypeevent, string message)
-        {
-            db.Open();
-            command.Connection = db._connection;
-            command.CommandText = $"INSERT INTO tab_total_log (id_type_event, date_time_event, user, message)" +
-                $"VALUES ('{idtypeevent}', '{DateTime.Now.ToString()}', '{Environment.UserName.ToString()}', '{message}')";
-            command.ExecuteNonQuery();
-            db.Close();
-        }
-
-        //Выводит данные таблицы в консоль на этапе отладки
-        public void GetTypesOfEvent()
-        {
-            db.Open();
-            var sql = "select * from tab_types_of_events";
-            using var result = db.SelectQuery(sql);
-
-            if (!result.HasRows)
-            {
-                Console.WriteLine("Нет данных");
-                return;
-            }
-
-            var types = new List<TypesOfEvent>();
-            while (result.Read())
-            {
-                var type = new TypesOfEvent
-                {
-                    Id = result.GetInt32(0),
-                    TypeEvent = result.GetString(1)
-                };
-                types.Add(type);
-            }
-
-            foreach (var type in types)
-            {
-                Console.WriteLine($"{type.Id} - {type.TypeEvent}");
-            }
-
             db.Close();
         }
 
@@ -83,7 +42,7 @@ namespace Logger
                 var total = new TotalLog
                 {
                     Id = result.GetInt32(0),
-                    IdTypeEvent = result.GetInt32(1),
+                    TypeEvent = result.GetString(1),
                     DateTimeEvent = result.GetString(2),
                     User = result.GetString(3),
                     Message = result.GetString(4)
@@ -93,7 +52,7 @@ namespace Logger
 
             foreach (var total in totals)
             {
-                Console.WriteLine($"{total.Id} | {total.IdTypeEvent} | {total.DateTimeEvent} | {total.User} | {total.Message}");
+                Console.WriteLine($"{total.Id} | {total.TypeEvent} | {total.DateTimeEvent} | {total.User} | {total.Message}");
             }
 
             db.Close();
